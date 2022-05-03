c0b2f03d1fa1f4d7ce6a927133f620d9b69b024a
Daniel Ashbrook <dan@danielashbrook.com>
2022-05-03 14:10:42 +0200

    bugfixes

--- env2hat.py
+++ env2hat.py
@@ -1,14 +1,14 @@
 import time
 from array import array
 from machine import I2C
-from bmm150_defs import *#CHIP_ID, CHIP_ID_ADDR, DATA_RATE_10HZ, DATA_RATE_20HZ, DATA_RHALL, DATA_X, DATA_X_LSB, DATA_Y, DATA_Z, DIG_X1, DIG_Z2_LSB, DIG_Z4_LSB, ENHANCED_REPXY, ENHANCED_REPZ, FORCED_MODE, HIGHACCURACY_REPXY, HIGHACCURACY_REPZ, I2C_Address, LOWPOWER_REPXY, LOWPOWER_REPZ, NORMAL_MODE, ODR, OP_MODE, OP_MODE_ADDR, POWER_CNTRL_DISABLE, POWER_CNTRL_ENABLE, POWER_CONTROL_ADDR, PRESETMODE_ENHANCED, PRESETMODE_HIGHACCURACY, PRESETMODE_LOWPOWER, PRESETMODE_REGULAR, PWR_CNTRL, REGULAR_REPXY, REGULAR_REPZ, REP_XY_ADDR, REP_Z_ADDR, SLEEP_MODE, START_UP_TIME, SUSPEND_MODE, XYZR_DATA_LEN, data_rate, dig_x1, dig_x2, dig_xy1, dig_xy2, dig_xyz1, dig_y1, dig_y2, dig_z1, dig_z2, dig_z3, dig_z4, xy_rep, z_rep
+from bmm150_defs import CHIP_ID, CHIP_ID_ADDR, DATA_RATE_10HZ, DATA_RATE_20HZ, DATA_RHALL, DATA_X, DATA_X_LSB, DATA_Y, DATA_Z, DIG_X1, DIG_Z2_LSB, DIG_Z4_LSB, ENHANCED_REPXY, ENHANCED_REPZ, FORCED_MODE, HIGHACCURACY_REPXY, HIGHACCURACY_REPZ, I2C_Address, LOWPOWER_REPXY, LOWPOWER_REPZ, NORMAL_MODE, ODR, OP_MODE, OP_MODE_ADDR, POWER_CNTRL_DISABLE, POWER_CNTRL_ENABLE, POWER_CONTROL_ADDR, PRESETMODE_ENHANCED, PRESETMODE_HIGHACCURACY, PRESETMODE_LOWPOWER, PRESETMODE_REGULAR, PWR_CNTRL, REGULAR_REPXY, REGULAR_REPZ, REP_XY_ADDR, REP_Z_ADDR, SLEEP_MODE, START_UP_TIME, SUSPEND_MODE, XYZR_DATA_LEN, data_rate, dig_x1, dig_x2, dig_xy1, dig_xy2, dig_xyz1, dig_y1, dig_y2, dig_z1, dig_z2, dig_z3, dig_z4, xy_rep, z_rep
 
 def set_bits_pos_0(reg_data, bitname, data):
 	return (reg_data & bitname[0]) | (data & bitname[0])
 
+
 def set_bits(reg_data, bitname, data):
-	return (reg_data & bitname[0]) | \
-			((data << bitname[1]) & bitname[0])
+	return (reg_data & bitname[0]) | ((data << bitname[1]) & bitname[0])
 
 
 def get_bits(reg_data, bitname):
@@ -30,29 +30,33 @@ def print_devices():
 
 
 class BMM150:
-	def __init__(self, i2c):
+	def __init__(self, i2c, calibrate_ms=10):
 		self.trim     = [0]*11
 		self.settings = [0]*7
-		self.mag_data = array('h', [0]*3)
-
 		self.reg_data = bytearray(1)
+
+		self.mag_data_raw = array('h', [0]*4)
+		self.mag_data     = array('h', [0]*3)
+		self.mag_offsets  = array('h', [0]*3)
+
+		self.calibrated = False
+
 
 		self.i2c = i2c
 		self.set_op_mode(SLEEP_MODE)
 		time.sleep_ms(START_UP_TIME)
 
 		self.i2c_read(CHIP_ID_ADDR, self.reg_data)
-		print("Got chip address: %s" % self.reg_data[0])
 		if self.reg_data[0] != CHIP_ID:
-			raise ValueError("But expected ID %s" % CHIP_ID)
+			raise ValueError("Got chip ID %s but expected %s" % (self.reg_data[0], CHIP_ID))
 
-		print("Successfully initialized")
+		print("BMM150 successfully initialized")
 
 		self.read_trim_registers()
-
 		self.set_op_mode(NORMAL_MODE)
-
 		self.set_presetmode(PRESETMODE_LOWPOWER)
+
+		self.calibrate(calibrate_ms)
 
 
 	def i2c_read(self, address, buffer):
@@ -90,9 +94,6 @@ class BMM150:
 
 	def set_power_control_bit(self, bit):
 		self.i2c_bit_op0(POWER_CONTROL_ADDR, PWR_CNTRL, bit)
-		# self.i2c_read1(POWER_CONTROL_ADDR)
-		# self.reg_data1[0] = set_bits_pos_0(self.reg_data1[0], PWR_CNTRL, bit)
-		# self.i2c_write(POWER_CONTROL_ADDR, self.reg_data1)
 
 
 	def write_op_mode(self, op_mode):
@@ -112,8 +113,6 @@ class BMM150:
 		self.i2c_read(DIG_X1,     trim_x1y1)
 		self.i2c_read(DIG_Z4_LSB, trim_xyz_data)
 		self.i2c_read(DIG_Z2_LSB, trim_xy1xy2)
-
-		print("trims:", trim_x1y1, trim_xyz_data, trim_xy1xy2)
 
 		#Update trim data in device structure
 		trim_data = self.trim
@@ -161,16 +160,8 @@ class BMM150:
 
 
 	def set_odr_xyz_rep(self):
-		#set_odr()
 		self.i2c_bit_op(OP_MODE_ADDR, ODR, self.settings[data_rate])
-		# reg_data = self.i2c_read(OP_MODE_ADDR)
-		# reg_data = set_bits(reg_data, ODR, self.settings[data_rate])
-		# self.i2c_write(OP_MODE_ADDR, reg_data)
-
-		#set_xy_rep()
 		self.i2c_write(REP_XY_ADDR, bytes([self.settings[xy_rep]]))
-
-		#set_z_rep()
 		self.i2c_write(REP_Z_ADDR, bytes([self.settings[z_rep]]))
 
 
@@ -180,18 +171,56 @@ class BMM150:
 		self.i2c_read(DATA_X_LSB, reg_data)
 
 		reg_data[0] = get_bits(reg_data[0], DATA_X)
-		raw_x = (reg_data[1]*32) | reg_data[0]
+		self.mag_data_raw[0] = (reg_data[1]*32) | reg_data[0]
 
 		reg_data[2] = get_bits(reg_data[2], DATA_Y)
-		raw_y = (reg_data[3]*32) | reg_data[2]
+		self.mag_data_raw[1] = (reg_data[3]*32) | reg_data[2]
 
 		reg_data[4] = get_bits(reg_data[4], DATA_Z)
-		raw_z = (reg_data[5]*128) | reg_data[4]
+		self.mag_data_raw[2] = (reg_data[5]*128) | reg_data[4]
 
 		reg_data[6] = get_bits(reg_data[6], DATA_RHALL)
-		raw_r = (reg_data[7] << 6) | reg_data[6]
+		self.mag_data_raw[3] = (reg_data[7] << 6) | reg_data[6]
 
-		return raw_x, raw_y, raw_z, raw_r
+		if self.calibrated:
+			self.mag_data[0] = self.mag_data_raw[0] - self.mag_offsets[0]
+			self.mag_data[1] = self.mag_data_raw[1] - self.mag_offsets[1]
+			self.mag_data[2] = self.mag_data_raw[2] - self.mag_offsets[2]
+			return self.mag_data
+
+		return self.mag_data_raw
+
+
+	def calibrate(self, timeout=10):
+		if timeout <= 0:
+			return
+
+		self.read_mag_data()
+
+		xmin = xmax = self.mag_data_raw[0]
+		ymin = ymax = self.mag_data_raw[1]
+		zmin = zmax = self.mag_data_raw[2]
+
+		time.sleep_ms(100)
+
+		start = time.ticks_ms()
+		while time.ticks_diff(time.ticks_ms(), start) < timeout:
+			self.read_mag_data()
+			xmin = min(xmin, self.mag_data_raw[0])
+			xmax = max(xmax, self.mag_data_raw[0])
+			ymin = min(ymin, self.mag_data_raw[1])
+			ymax = max(ymax, self.mag_data_raw[1])
+			zmin = min(zmin, self.mag_data_raw[2])
+			zmax = max(zmax, self.mag_data_raw[2])
+
+		time.sleep_ms(1)
+
+		self.mag_offsets[0] = int(xmin + (xmax - xmin) / 2)
+		self.mag_offsets[1] = int(ymin + (ymax - ymin) / 2)
+		self.mag_offsets[2] = int(zmin + (zmax - zmin) / 2)
+
+		self.calibrated = True
+
 
 
 def run():
